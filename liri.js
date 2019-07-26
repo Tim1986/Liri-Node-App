aeaa6dbd6b04b31d6c048fd140f5ed9784002044
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-07-26 19:03:21 -0400

    functionality working, messy solutions, needs more work

--- liri.js
+++ liri.js
@@ -1,89 +1,119 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
-
 var axios = require("axios");
-
+var fs = require("fs");
 var inputString = process.argv;
 var command = inputString[2];
-var search = inputString[3];
-var output;
 
 switch (command) {
     case "concert-this":
-        var concertName = process.argv.slice(3).join("+")
-        var concertQueryUrl = `https://rest.bandsintown.com/artists/${concertName}/events?app_id=codingbootcamp`
-
-        axios.get(concertQueryUrl).then(
-            function (response) {
-
-                for (var i = 0; i < response.data.length; i++) {
-                    var concertObject = {
-                        venueName: response.data[i].venue.name,
-                        venueLocation: response.data[i].venue.city,
-                        eventDate: response.data[i].datetime
-                    }
-                    for (var key in concertObject) {
-                        console.log('* ' + concertObject[key]);
-                    }
-                }
-            })
+        concert();
         break;
 
     case "spotify-this-song":
-        var keys = require("./keys.js");
-        var spotify = new Spotify(keys.spotify);
-
-        spotify
-            .search({ type: 'track', query: process.argv.slice(3).join("+")})
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
-// NEED TO STRINGIFY THE OBJECTS
-
-        // var songName = process.argv.slice(3).join("+")
-        // var songQueryUrl = `https://rest.bandsintown.com/artists/${concertName}/events?app_id=codingbootcamp`
-        // axios.get(movieQueryUrl).then(
-        //     function (response) {
-        //         var songObject = {
-
-        //         }
-        //     }
-      
-
-      break;
-
-    case "movie-this":
-
-        var movieName = process.argv.slice(3).join("+")
-        var movieQueryUrl = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`
-
-        axios.get(movieQueryUrl).then(
-            function (response) {
-                var movieObject = {
-                    title: response.data.Title,
-                    year: response.data.Year,
-                    imdbRating: response.data.imdbRating,
-                    rottenTomatoesRating: response.data.Ratings[1].Value,
-                    country: response.data.Country,
-                    language: response.data.Language,
-                    plot: response.data.Plot,
-                    actors: response.data.Actors,
-                }
-                for (var key in movieObject) {
-                    console.log('* ' + movieObject[key]);
-                }
-            })
+        song();
         break;
 
-    // case "do-what-it-says":
-    //   outputNum = 
-    //   break;
+    case "movie-this":
+        movie();
+        break;
+
+    case "do-what-it-says":
+        backstreet();
+        break;
 
     default:
         output = "Not a recognized command";
 }
 
+function concert() {
+    var concertName = process.argv.slice(3).join("+")
+    var concertQueryUrl = `https://rest.bandsintown.com/artists/${concertName}/events?app_id=codingbootcamp`
+
+    axios.get(concertQueryUrl).then(
+        function (response) {
+
+            for (var i = 0; i < response.data.length; i++) {
+                var concertObject = {
+                    venueName: response.data[i].venue.name,
+                    venueLocation: response.data[i].venue.city,
+                    eventDate: response.data[i].datetime
+                }
+                for (var key in concertObject) {
+                    console.log('* ' + concertObject[key]);
+                }
+            }
+        })
+}
+
+function song() {
+    var keys = require("./keys.js");
+    var spotify = new Spotify(keys.spotify);
+
+    spotify
+        .search({ type: 'track', query: process.argv.slice(3).join("+"), limit: 1 })
+        .then(function (response) {
+            var songObject = {
+                artist: response.tracks.items[0].artists[0].name,
+                song: response.tracks.items[0].name,
+                preview: response.tracks.items[0].preview_url,
+                album: response.tracks.items[0].album.name
+            }
+            for (var key in songObject) {
+                console.log('* ' + songObject[key])
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+function movie() {
+    var movieName = process.argv.slice(3).join("+")
+    var movieQueryUrl = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`
+
+    axios.get(movieQueryUrl).then(
+        function (response) {
+            var movieObject = {
+                title: response.data.Title,
+                year: response.data.Year,
+                imdbRating: response.data.imdbRating,
+                rottenTomatoesRating: response.data.Ratings[1].Value,
+                country: response.data.Country,
+                language: response.data.Language,
+                plot: response.data.Plot,
+                actors: response.data.Actors,
+            }
+            for (var key in movieObject) {
+                console.log('* ' + movieObject[key]);
+            }
+        })
+}
+
+function backstreet() {
+    var keys = require("./keys.js");
+    var spotify = new Spotify(keys.spotify);
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error)
+        }
+        var adjustedData = data.split(",")
+
+            spotify
+                .search({ type: 'track', query: adjustedData[1], limit: 1 })
+                .then(function (response) {
+                    var songObject = {
+                        artist: response.tracks.items[0].artists[0].name,
+                        song: response.tracks.items[0].name,
+                        preview: response.tracks.items[0].preview_url,
+                        album: response.tracks.items[0].album.name
+                    }
+                    for (var key in songObject) {
+                        console.log('* ' + songObject[key])
+                    }
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+    })
+}
